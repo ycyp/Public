60750eeed48dde670f5bd7a644c617bc38495f77
ChengYen Yang <ycyp.tw@gmail.com>
2025-09-24 18:24:23 +0800

    Create helloworlduefi.rs

---
+++ Rust/helloworlduefi.rs
@@ -0,0 +1,14 @@
+```
+#![no_main]
+#![no_std]
+
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+    info!("Hello world!");
+    boot::stall(10_000_000);
+    Status::SUCCESS
+}
